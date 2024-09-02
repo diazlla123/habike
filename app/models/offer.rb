09372b5695c63aeb40b87d4b098823909b24b130
Andres Diaz <diazlla123@gmail.com>
2024-09-02 14:50:23 -0600

    Creación de base de datos

---
+++ app/models/offer.rb
@@ -0,0 +1,4 @@
+class Offer < ApplicationRecord
+  belongs_to :bike
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_02_204805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bikes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "model"
+    t.text "description"
+    t.integer "kilometers"
+    t.decimal "price"
+    t.string "category"
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.text "message"
+    t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_chats_on_offer_id"
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string "status"
+    t.bigint "bike_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_offers_on_bike_id"
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "chats", "offers"
+  add_foreign_key "chats", "users"
+  add_foreign_key "offers", "bikes"
+  add_foreign_key "offers", "users"
 end
