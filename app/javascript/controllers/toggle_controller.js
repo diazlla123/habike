c9daeba343572408d8c35d19013fc53c10e378a6
Corentin Dubois <corentindubois22@gmail.com>
2024-09-05 18:45:42 -0600

    added the toggle logic and styling in the profile page

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,15 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["displayedBikes"]
+  static targets = ["postedBikes", "purchasedBikes", "postedButton", "purchasedButton"]
 
   connect() {
     console.log("hi mom!")
   }
 
-  display() {
-    console.log(this.displayedBikesTarget.classList)
-    console.log(this.displayedBikesTarget.childrenNodes)
-    this.displayedBikesTarget.classList.toggle("active");
+  displayPosted() {
+    this.postedBikesTarget.classList.remove("d-none");
+    this.purchasedBikesTarget.classList.add("d-none");
+    this.postedButtonTarget.classList.add("active");
+    this.purchasedButtonTarget.classList.remove("active");
+  }
+
+  displayPurchased() {
+    this.purchasedBikesTarget.classList.remove("d-none")
+    this.postedBikesTarget.classList.add("d-none");
+    this.postedButtonTarget.classList.remove("active");
+    this.purchasedButtonTarget.classList.add("active");
   }
 }
